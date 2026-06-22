e4435b7c5744f75cb81dea8963a5b9b0749aabe1
Sikander Atif <sikanderatif07@gmail.com>
2026-06-22 13:38:53 +0500

    Database Selection & Initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final database = openDatabase(
+    join(await getDatabasesPath(), 'expense_database.db'),
+    onCreate: (db, version) {
+      return db.execute(
+        'CREATE TABLE Expenses (ID INTEGER PRIMARY KEY, Name TEXT, Description TEXT, Amount INTEGER)',
+      );
+    },
+    version: 1,
+  );
+
+  final db = await database;
+  Map<String, dynamic> row = {
+    'Name': 'Honda Civic 2017',
+    'Description': 'Bought used Honda Civic 2017 model which is just like new.',
+    'Amount': 5000000,
+  };
+  await db.insert('Expenses', row);
+  List<Map<String,dynamic>> expenses = await db.query(
+    'Expenses'
+  );
+  print(expenses);
+  db.close();
+
   runApp(const MainApp());
 }
 
