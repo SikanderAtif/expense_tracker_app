99dd95f6261d2a8294a7d53a3a685500fa474165
Sikander Atif <sikanderatif07@gmail.com>
2026-07-15 20:20:09 +0500

    Removed duplicate button in Budget Page empty state. Integrated Firebase settings for future implementation.

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'theme/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:expense_tracker_app/services/expense_helper.dart';
 import 'screens/home_screen.dart';
 import 'l10n/app_localizations.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await ExpensesHelper.init();
 
   runApp(const MainApp());
